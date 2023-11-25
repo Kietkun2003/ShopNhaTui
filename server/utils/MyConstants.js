a81c49d8d9849d6afdccfbe08580dad4d9f05797
Kietkun2003 <93863161+Kietkun2003@users.noreply.github.com>
2023-11-25 22:11:48 +0700

    upload server

---
+++ server/utils/MyConstants.js
@@ -0,0 +1,12 @@
+const MyConstants = {
+    DB_SERVER: 'cluster01.nqeauke.mongodb.net',
+    DB_USER: 'tuankiet153',
+    DB_PASS: '123',
+    DB_DATABASE: 'Tiemnhakit',
+    // DB_DATABASE: 'shoppingonline',
+    JWT_SECRET: '442',
+    JWT_EXPIRES: '3516360000000000000', // in milliseconds
+    EMAIL_USER: 'dongngoctuankiet153@gmail.com', // gmail service
+    EMAIL_PASS: 'dbbhrrmwajvvibjx'
+  };
+  module.exports = MyConstants; 
